de3b7f7f7aa45612ec912014bd6568f49fc64664
TruongCongTri <congtritruong1994@gmail.com>
2026-06-01 00:32:19 +0700

    update whitelist

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.set('trust proxy', 1);
  */
 app.use(helmet());
 
-const whitelist = [env.CLIENT_URL];
+const whitelist = env.CLIENT_URL.split(',');
 app.use(
   cors({
     origin: function (origin, callback) {
